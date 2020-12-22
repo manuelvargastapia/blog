1fecad5d40af475c8dd4880d33a4e897f46c8c2f
manuelvargastapia <manuelvargastapia@gmail.com>
2020-12-21 22:38:21 -0300

    show all blogs in home page

---
+++ pages/_slug.vue
@@ -0,0 +1,16 @@
+<template>
+  <article>
+    <nuxt-content :document="article" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('blog', params.slug).fetch()
+    return { article }
+  },
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Blog</h1>
+  <v-col>
+    <v-card v-for="article of articles" :key="article">
+      <nuxt-link :to="{ name: 'slug', params: { slug: article.slug } }">
+        <img :src="require(`assets/resources/${article.img}`)" alt="" />
+        <h3>{{ article.title }}</h3>
+        <p>{{ article.description }}</p>
+      </nuxt-link>
+    </v-card>
+  </v-col>
 </template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const articles = await $content('blog', params.slug)
+      .only(['title', 'description', 'img', 'slug'])
+      .sortBy('createdAt', 'asc')
+      .fetch()
+
+    return { articles }
+  },
+}
+</script>
