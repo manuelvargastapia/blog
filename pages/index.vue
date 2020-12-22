f79ea5da645bc120a1b22f2a5eebd56b56589c41
manuelvargastapia <manuelvargastapia@gmail.com>
2020-12-22 20:05:20 -0300

    create component to render the most recent article card

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,11 @@
 <template>
-  <v-container>
-    <v-card v-for="article of articles" :key="article">
-      <nuxt-link :to="article.dir">
-        <v-img
-          v-if="article.featured"
-          :src="require(`static/featured_images/${article.featured}`)"
-          :alt="article.title"
-        ></v-img>
-        <h3>{{ article.title }}</h3>
-        <p>{{ article.description }}</p>
-      </nuxt-link>
-    </v-card>
-  </v-container>
+  <last-article-card :articles="articles" />
 </template>
 
 <script>
+import LastArticleCard from '~/components/LastArticleCard.vue'
 export default {
+  components: { LastArticleCard },
   async asyncData({ $content }) {
     const articles = await $content({ deep: true })
       .only(['title', 'description', 'slug', 'dir', 'featured'])
