f54b5ded7dd19b7b6e09d6e4ee95bda8910e08dc
manuelvargastapia <manuelvargastapia@gmail.com>
2020-12-26 20:28:14 -0300

    add apps page

---
+++ components/AppCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <v-card class="mx auto" max-width="1080" max-height="500" elevation="0">
+    </v-card>
+    <v-responsive max-width="1080">
+      <div class="pa-3 secondary rounded-xl">
+        <v-img
+          v-if="app.featured"
+          :src="require(`static/featured_images/${app.featured}`)"
+          :alt="app.name"
+          class="rounded-image"
+          min-width="100"
+          contain
+        ></v-img>
+      </div>
+      <v-container fluid>
+        <v-card-title class="headline" v-text="app.name"> </v-card-title>
+        <v-card-subtitle v-text="app.preview"> </v-card-subtitle>
+        <v-row>
+          <v-col align="center">
+            <v-btn v-if="app.repo" :href="app.repo" target="_blank" text>
+              <v-card-text>Repositorio</v-card-text>
+            </v-btn>
+            <v-btn
+              v-if="app.download"
+              :href="app.download"
+              target="_blank"
+              text
+            >
+              <v-card-text>Descarga</v-card-text>
+            </v-btn>
+            <nuxt-link v-if="app.article_slug" :to="app.article_slug">
+              <v-btn text>
+                <v-card-text>Artículo</v-card-text>
+              </v-btn>
+            </nuxt-link>
+            <v-btn
+              v-if="app.reference"
+              :href="app.reference"
+              target="_blank"
+              text
+            >
+              <v-card-text>Referencia</v-card-text>
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-card-actions>
+          <v-card-action>
+            <v-btn icon>
+              <v-icon>mdi-heart-outline</v-icon>
+            </v-btn>
+          </v-card-action>
+          <v-card-action>
+            <v-btn icon>
+              <v-icon>mdi-comment-outline</v-icon>
+            </v-btn>
+          </v-card-action>
+          <v-card-action>
+            <v-btn icon>
+              <v-icon>mdi-share-variant-outline</v-icon>
+            </v-btn>
+          </v-card-action>
+        </v-card-actions>
+      </v-container>
+    </v-responsive>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    app: {
+      type: [Object],
+      required: true,
+    },
+  },
+}
+</script>
---
+++ components/AppsList.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container fill-height>
+    <v-row justify="center" align="center">
+      <v-col v-for="app in apps" :key="app.title" cols="auto">
+        <app-card :app="app"></app-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    apps: {
+      type: [Array],
+      required: true,
+    },
+  },
+}
+</script>
