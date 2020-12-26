b48bb80b89b26e2329dd6bda091b0e9f5c7ab27a
manuelvargastapia <manuelvargastapia@gmail.com>
2020-12-26 17:35:25 -0300

    improve nav items responsiveness

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,17 +3,44 @@
     <v-app-bar app flat absolute color="transparent">
       <v-toolbar-title>Manuel Vargas</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon @click="$vuetify.theme.dark = !$vuetify.theme.dark">
-        <v-icon>mdi-brightness-4</v-icon>
-      </v-btn>
-      <v-btn to="/" nuxt text> Blog </v-btn>
-      <v-btn to="/apps" nuxt text> Apps </v-btn>
-      <v-btn to="/about" nuxt text> Acerca de </v-btn>
+      <div class="d-none d-md-block">
+        <v-container v-for="align in alignments" :key="align">
+          <v-row align="center">
+            <v-btn icon @click="$vuetify.theme.dark = !$vuetify.theme.dark">
+              <v-icon>mdi-brightness-4</v-icon>
+            </v-btn>
+            <v-col v-for="(item, index) in navItems" :key="index">
+              <v-btn :to="item.to" nuxt text>
+                {{ item.title }}
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
+      <div class="d-md-none">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <font-awesome-icon :icon="faBars" />
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item>
+              <v-btn icon @click="$vuetify.theme.dark = !$vuetify.theme.dark">
+                <v-icon>mdi-brightness-4</v-icon>
+              </v-btn>
+            </v-list-item>
+            <v-list-item v-for="(item, index) in navItems" :key="index">
+              <v-list-item-title
+                ><v-btn :to="item.to" nuxt text>
+                  {{ item.title }}
+                </v-btn></v-list-item-title
+              >
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
     </v-app-bar>
-    <!-- 
-    MOBILE
-    <v-navigation-drawer app>
-    </v-navigation-drawer> -->
     <v-main>
       <v-container fluid>
         <nuxt />
@@ -72,8 +99,19 @@ import {
   faMedium,
   faInstagram,
 } from '@fortawesome/free-brands-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default {
+  data() {
+    return {
+      navItems: [
+        { title: 'Blog', to: '/' },
+        { title: 'Apps', to: 'apps' },
+        { title: 'Acerca de', to: 'about' },
+      ],
+      alignments: ['center'],
+    }
+  },
   computed: {
     faGithub() {
       return faGithub
@@ -86,6 +124,9 @@ export default {
     },
     faInstagram() {
       return faInstagram
+    },
+    faBars() {
+      return faBars
     },
   },
 }
