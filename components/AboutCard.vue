075696e5f2609c43c263407ec2cf98ba5a723504
manuelvargastapia <manuelvargastapia@gmail.com>
2020-12-30 19:02:41 -0300

    add bouit page content

---
+++ components/AboutCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card class="mx auto" max-width="800" elevation="0">
+    <v-card-title>{{ aboutInfo.title }}</v-card-title>
+    <v-card-text>{{ aboutInfo.content }}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    aboutInfo: {
+      type: [Object],
+      required: true,
+    },
+  },
+}
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>About</h1>
+  <v-container fill-height>
+    <v-row justify="center" align="center">
+      <v-col cols="auto">
+        <about-card :about-info="aboutInfo"></about-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import { getPageTable } from 'vue-notion'
+
+export default {
+  async asyncData() {
+    const aboutInfo = await getPageTable(
+      '0940a94496fc4adfbb0c73af221ab8b7',
+      'https://notion-cloudflare-worker.manuelvargastapia.workers.dev/v1'
+    )
+    return { aboutInfo: aboutInfo[0] }
+  },
+}
+</script>
