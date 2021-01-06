bd922a26703d779e922974f0a33076b6ba6c8929
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-06 14:13:06 -0300

    improve responsiveness

--- components/NextArticleCard.vue
+++ components/NextArticleCard.vue
@@ -1,48 +1,60 @@
 <template>
   <div>
-    <v-card class="mx auto" max-width="600" max-height="500" elevation="0">
-    </v-card>
-    <div
-      :class="{ 'd-flex': $vuetify.breakpoint.lg }"
-      class="flex-no-wrap justify-space-between"
+    <v-card
+      :class="{
+        'd-flex': $vuetify.breakpoint.lg,
+        'pa-4': $vuetify.breakpoint.mdAndDown,
+        'px-4': $vuetify.breakpoint.lg,
+      }"
+      class="mx-auto justify-space-between align-center"
+      max-width="600"
+      max-height="500"
+      elevation="0"
+      color="#3a3a3a"
     >
-      <v-responsive :max-width="$vuetify.breakpoint.lg ? 300 : 400">
-        <div class="pa-3 secondary rounded">
-          <v-img
-            v-if="article.featured"
-            :src="require(`static/featured_images/${article.featured}`)"
-            :alt="article.title"
-            class="rounded"
-            min-width="100"
-            contain
-          ></v-img>
-        </div>
-      </v-responsive>
-      <v-container fluid class="d-flex flex-column justify-center">
-        <nuxt-link :to="article.slug">
-          <v-card-title class="headline" v-text="article.title"> </v-card-title>
-        </nuxt-link>
-        <v-card-subtitle v-text="article.preview"> </v-card-subtitle>
-        <v-card-actions class="pr-md-3 pl-sm-6">
-          <v-row justify="space-between" align="center">
-            <div>
-              <v-btn icon>
-                <v-icon>mdi-heart-outline</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-comment-outline</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-share-variant-outline</v-icon>
-              </v-btn>
-            </div>
-            <div>
-              <v-card-text v-text="article.date" />
-            </div>
-          </v-row>
-        </v-card-actions>
-      </v-container>
-    </div>
+      <div
+        :class="{ 'd-flex': $vuetify.breakpoint.lg }"
+        class="flex-no-wrap justify-space-between align-center"
+      >
+        <v-responsive :max-width="$vuetify.breakpoint.lg ? 250 : 400">
+          <div class="secondary rounded">
+            <v-img
+              v-if="article.featured"
+              :src="require(`static/featured_images/${article.featured}`)"
+              :alt="article.title"
+              class="rounded"
+              min-width="100"
+              contain
+            ></v-img>
+          </div>
+        </v-responsive>
+        <v-container fluid class="d-flex flex-column justify-center">
+          <nuxt-link :to="article.slug">
+            <v-card-title class="headline-3" v-text="article.title">
+            </v-card-title>
+          </nuxt-link>
+          <v-card-subtitle v-text="article.preview"> </v-card-subtitle>
+          <v-card-actions class="pr-md-3 pl-sm-6">
+            <v-row justify="space-between" align="center">
+              <div>
+                <v-btn icon>
+                  <v-icon>mdi-heart-outline</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>mdi-comment-outline</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>mdi-share-variant-outline</v-icon>
+                </v-btn>
+              </div>
+              <div>
+                <v-card-text v-text="article.date" />
+              </div>
+            </v-row>
+          </v-card-actions>
+        </v-container>
+      </div>
+    </v-card>
   </div>
 </template>
 
@@ -56,3 +68,9 @@ export default {
   },
 }
 </script>
+
+<style>
+.v-card__title {
+  word-break: normal;
+}
+</style>
