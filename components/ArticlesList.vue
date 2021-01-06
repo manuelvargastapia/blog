42a4824a816dc9fec89ea10a0957b4e3362d5018
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-06 15:02:54 -0300

    remove v-row

--- components/ArticlesList.vue
+++ components/ArticlesList.vue
@@ -6,15 +6,13 @@
           :article="articles[0]"
         ></most-recent-article-card>
       </v-col>
-      <v-row>
-        <v-col
-          v-for="article in articles.slice(1)"
-          :key="article.title"
-          :cols="$vuetify.breakpoint.mdAndUp ? 6 : `auto`"
-        >
-          <next-article-card :article="article"></next-article-card>
-        </v-col>
-      </v-row>
+      <v-col
+        v-for="article in articles.slice(1)"
+        :key="article.title"
+        :cols="$vuetify.breakpoint.mdAndUp ? 6 : `auto`"
+      >
+        <next-article-card :article="article"></next-article-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
