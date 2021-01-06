39f3d0b164843ffe61364b17a4c7e43c8c52a50b
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-06 14:12:51 -0300

    get about page from Notion

--- pages/about.vue
+++ pages/about.vue
@@ -1,23 +1,42 @@
 <template>
-  <v-container fill-height>
-    <v-row justify="center" align="center">
-      <v-col cols="auto">
-        <about-card :about-info="aboutInfo"></about-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <notion-renderer
+    :block-map="blockMap"
+    :page-link-options="pageLinkOptions"
+    full-page
+  />
 </template>
 
 <script>
-import { getPageTable } from 'vue-notion'
+import { NotionRenderer, getPageBlocks, getPageTable } from 'vue-notion'
 
 export default {
-  async asyncData() {
-    const aboutInfo = await getPageTable(
+  components: { NotionRenderer },
+  async asyncData({ error }) {
+    const pageTable = await getPageTable(
       '0940a94496fc4adfbb0c73af221ab8b7',
       'https://notion-cloudflare-worker.manuelvargastapia.workers.dev/v1'
     )
-    return { aboutInfo: aboutInfo[0] }
+    const blockMap = await getPageBlocks(
+      pageTable[0].id,
+      'https://notion-cloudflare-worker.manuelvargastapia.workers.dev/v1'
+    )
+    if (!blockMap || blockMap.error) {
+      return error({ statusCode: 404, message: 'Post not found' })
+    }
+    return { blockMap }
+  },
+  data() {
+    return {
+      pageLinkOptions: { components: 'NuxtLink', href: 'to' },
+    }
   },
 }
 </script>
+
+<style>
+@import 'vue-notion/src/styles.css';
+
+.notion {
+  color: rgb(255, 255, 255);
+}
+</style>
