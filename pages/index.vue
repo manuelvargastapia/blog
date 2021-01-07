db27e3c55f39ccad621d828078acada55ed4a5a7
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-06 21:12:18 -0300

    order by default

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@ export default {
       '1c810a1cdb3a497f83e4bdc779e2377e',
       'https://notion-cloudflare-worker.manuelvargastapia.workers.dev/v1'
     )
-    const articles = pageTable.filter((page) => page.published).reverse()
+    const articles = pageTable.filter((page) => page.published)
     return { articles }
   },
 }
