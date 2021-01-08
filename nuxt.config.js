97a4f408a6844f6271c4c05ed5b08d441934a3f8
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-08 19:22:52 -0300

    add favicon and remove unused logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,14 +6,14 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - blog',
-    title: 'blog',
+    titleTemplate: '%s - Blog',
+    title: 'Manuel Vargas',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/logo_light.svg' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
