e18100ea8d55137ce1eb003be701def49a0b3146
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-08 21:49:04 -0300

    adjust icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/logo_light.svg' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/fav_icon.svg' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
