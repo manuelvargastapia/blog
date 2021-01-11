822b4bc4a8d23d7b0dbb811188adb3428dd674f4
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-11 16:00:52 -0300

    edit titleTemplate

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - Blog',
+    titleTemplate: '%s',
     title: 'Manuel Vargas',
     meta: [
       { charset: 'utf-8' },
